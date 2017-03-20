97422e7882a302162a8f14bdcebc95b70409857b
Ferry Ariawan <ferryyangpuny@gmail.com>
2017-03-20 21:59:23 +0700

    init

---
+++ database/migrations/2017_03_17_085044_create_orders.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('customers_id')->nullable();
+            $table->string('order_number',25)->nullable();
+            
+            $table->double('total');
+            $table->double('tax');
+            $table->double('discount');
+            $table->double('grand_total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
